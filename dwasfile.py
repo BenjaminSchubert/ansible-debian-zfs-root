6ef556152e1b9dc6ba977a1c30892b6c00ae6963
Benjamin Schubert <contact@benschubert.me>
2025-12-19 18:30:05 +0100

    Update to use newer ansible and modernize the stack

---
+++ dwasfile.py
@@ -0,0 +1,28 @@
+from dwas import managed_step, StepRunner
+
+
+@managed_step(
+    ["-rrequirements.txt", "ansible-lint"],
+    name="ansible-lint",
+)
+def ansible_lint(step: StepRunner) -> None:
+    env = {
+        "ANSIBLE_HOME": step.cache_path / "ansible",
+        "HOME": str(step.cache_path / "home"),
+    }
+
+    step.run(
+        [
+            "ansible-galaxy",
+            "install",
+            "--role-file=requirements.yml",
+            "--force",
+        ],
+        env=env,
+    )
+
+    command = ["ansible-lint", "--strict", "--offline"]
+    if step.config.colors:
+        command.append("--force-color")
+
+    step.run(command, env=env)
